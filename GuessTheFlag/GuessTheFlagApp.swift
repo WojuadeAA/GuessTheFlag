4856ae87c9a6edf9375aa25de3246358103ee521
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-12-11 17:27:22 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Wojuade Abdul Afeez on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
